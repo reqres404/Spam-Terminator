c0247263c8090f854030f48674f47076f3d2625f
reqres404 <adittyapatil78@gmail.com>
2023-06-25 19:10:34 +0530

    title changed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import PromptSpace from './components/promptSpace/PromptSpace';
 function App() {
   return (
     <div className="App">
-      <h1>Spam-Terminator</h1>
+      <h1>Spam Terminator</h1>
       <PromptSpace/>
     </div>
   );
